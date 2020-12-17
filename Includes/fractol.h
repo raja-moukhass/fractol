2cdbf9247eb0df90bfb064773bcacbd3c4932525
Raja Moukhass <53265154+raja-moukhass@users.noreply.github.com>
2020-12-17 19:12:00 +0100

    Update fractol.h

--- Includes/fractol.h
+++ Includes/fractol.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   fractol.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ramoukha <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: ramoukha <ramoukha@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/15 13:06:46 by ramoukha          #+#    #+#             */
-/*   Updated: 2020/12/15 13:06:48 by ramoukha         ###   ########.fr       */
+/*   Updated: 2020/12/17 18:59:56 by ramoukha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,21 @@
 # include "mlx.h"
 # include "math.h"
 # include "pthread.h"
-# define SQR(x) (x * x)
 # define WIDTH 600
 # define HEIGHT 600
 # define THREAD_WIDTH 5
 # define THREAD_NUMBER 120
+# define EXIT 53
+# define ITERATION_PLUS 69
+# define ITERATION_MINUS 78
+# define LEFT 123
+# define RIGHT 124
+# define BOTTOM 125
+# define TOP 126
+# define C_INSIDE 34
+# define C_OUTSIDE 8
+# define SPACE 49
+# define ACTIVED_MOUSE 1
 
 typedef struct	s_mlx
 {
@@ -52,7 +62,9 @@ typedef struct	s_mlx
 	double		tmp;
 	int			red;
 	int			color_in;
+	int			stop_keyboard;
 }				t_mlx;
+
 void			show_usage(void);
 void			all_init(t_mlx *data);
 
@@ -67,13 +79,12 @@ int				mouse_hook(int mousecode, int x, int y, t_mlx *data);
 void			mandelbrot_math(t_mlx *data);
 void			*mandelbrot(void *tab);
 void			mandelbrot_pthread(t_mlx *data);
-void    		put_pixel_image(t_mlx *data);
+void			put_pixel_image(t_mlx *data);
 
 int				mouse_julia(int x, int y, t_mlx *data);
 void			julia_math(t_mlx *data);
 void			*julia(void *tab);
 void			julia_pthread(t_mlx *data);
-
 
 void			burningship_math(t_mlx *data);
 void			*burningship(void *tab);
@@ -85,6 +96,6 @@ void			put_text(t_mlx *data);
 
 void			fract_calc(t_mlx *data);
 void			fract_init(t_mlx *data);
-void			mlx_win_init(t_mlx *data);
+void			mlx_win_init(t_mlx *data, char *title);
 void			fract_comp(char **av, t_mlx *data);
 #endif
